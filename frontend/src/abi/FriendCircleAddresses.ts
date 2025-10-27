f026c02d8321b083a3a002aee27133b786021927
norlel <norlel@proton.me>
2025-10-27 20:33:11 +0100

    Add FriendCircleAddresses.ts

---
+++ frontend/src/abi/FriendCircleAddresses.ts
@@ -0,0 +1,4 @@
+export const FriendCircleAddresses = {
+  "11155111": { address: "0xFc8D3C0DAebD0A34F7A5401A4e4eC36c0Cc270d4", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0", chainId: 31337, chainName: "hardhat" },
+};
